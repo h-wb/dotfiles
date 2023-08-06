7d2e9b918d573c17027c084d9b99c93e9e65069d
h-wb <dev.github@wlab.ovh>
2023-08-06 17:01:12 -0400

    add aliases

--- home/dot_config/zsh/aliases.zsh
+++ home/dot_config/zsh/aliases.zsh
@@ -1,10 +1,13 @@
 alias library="cd $HOME/Library"
 alias repo="cd $HOME/Repositories"
+alias dotfiles="code $HOME/Repositories/.local/share/chezmoi"
 
 alias ez="exec zsh"
 
 #Git
 alias g="git"
+alias galias="git config --get-regexp alias"
 
 #Chezmoi
 alias cm="chezmoi"
+
