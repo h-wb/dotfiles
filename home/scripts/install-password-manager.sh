ec980f1f3c673b0129882417a286920a7722a5d3
h-wb <>
2025-01-07 23:42:16 +0900

    clean up

--- home/scripts/install-password-manager.sh
+++ home/scripts/install-password-manager.sh
@@ -1,7 +1,6 @@
-# #!/bin/sh
+#!/bin/sh
 
 # Ensure bitwarden secrets manager (bws) is available
 # exit immediately if bws is available
 type bws >/dev/null 2>&1 && exit
-
 mise use -gy bitwarden-secrets-manager
